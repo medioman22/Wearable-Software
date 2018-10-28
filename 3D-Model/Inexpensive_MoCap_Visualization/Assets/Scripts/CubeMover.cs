6e34d1818fbc62110554a8b6e364a6185c9a8078
victor <victor.faraut@gmail.com>
2018-10-28 17:42:14 +0100

    visualization tool in Unity 3d
    For now basic visu with a cube for one IMU

---
+++ 3D-Model/Inexpensive_MoCap_Visualization/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour {
+
+    private Vector3 currentRotVect = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void SetAngleXYZ(int euler, float angle)
+    {
+        if (euler == 0)
+            currentRotVect[1] = angle;
+        if (euler == 1)
+            currentRotVect[0] = angle;
+        if (euler == 2)
+            currentRotVect[2] = -angle;
+    }
+    private void Move()
+    {
+        transform.eulerAngles = currentRotVect;
+    }
+}
